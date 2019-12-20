4b2697d6a65e4416d4cb8305589279768ed02f01
Eduardo <ing.valen0421@hotmail.com>
2019-12-20 13:34:52 -0400

    Update linear regression

--- UnderstandingTheLinearRegressionPart2.R
+++ UnderstandingTheLinearRegressionPart2.R
@@ -31,7 +31,28 @@ summary(regresion)
 # El intercepto tiene una significancia del 99%
 # La pendiente (edad) aun mas significativo el 99.9%
 
-#Es primordial, ya que observamos 
+#Es primordial, ya que nos ofrece una pauta para continuar con nuestro analisis
+#Tambien observamos el R-square que es de un 70%, tambien llamado el Coeficiente de determinacion
+#Quiere decir que la variabilidad entre Y y X (Grasa y Edad), es menor, es bueno y nos damos cuenta
+# con esto de que los puntos no distan mucho de la linea de regresion sin haberla creado ya.
+
+#Tambien podemos realizar un analisis ANOVA, el Analysis of the Variance
+#Esto nos da la BONDAD DE AJUSTE, si es significativa, se puede tener seguridad de que los datos son buenos usables
+#Para la prediccion
+anova(regresion)
+
+#Si es significativa, la Hipotesis nula H0, se rechaza y la Alternativa se acoge H1 como cierta
+#Que hay una correlacion diferente de cero
+
+
+#Grafico de dispersion
+plot(grasas$edad, grasas$grasas, xlab = "Edad", ylab = "Grasas")
+
+#la recta correspondiente a la funcion de dicha grafico
+abline(regresion)
+
+#Se nota una correlacion positiva, pero hay un 30% que no esta siendo explicado por este modelo, por el 70% del R^2 
+
 
 
 
