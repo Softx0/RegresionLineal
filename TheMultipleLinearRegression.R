fd2b2b30a732220f69e83177f9d93293735eec8a
Eduardo <ing.valen0421@hotmail.com>
2019-12-23 13:34:38 -0400

    Update

--- TheMultipleLinearRegression.R
+++ TheMultipleLinearRegression.R
@@ -1,2 +1,12 @@
 
 
+Advertising <- read.csv("/Users/Mac/Desktop/UNIVERSIDAD/Autodidacta/AlgoritmosAvanzados/RegresionLineal/Resources/advertising.csv")
+Advertising
+
+plot(Advertising)
+ModelAdvertising <- lm(Sales ~ Radio + TV + Newspaper, data = Advertising)
+
+
+summary(ModelAdvertising)   
+
+
